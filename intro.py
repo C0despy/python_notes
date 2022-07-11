bb8956b3c1fde8dd7471b8f5396e736c3f2506e5
a4akhare@gmail.com <a4akhare@gmail.com>
2022-07-11 10:32:08 +0530

    envope this

---
+++ intro.py
@@ -0,0 +1,27 @@
+
+   ##Introduction To Python##
+   
+What Is Python?
+1] Python is a high-level, interpreted, general-purpose programming language.
+2] Being a general-purpose language, it can be used to built almost any type of application with right tools/libraries.
+3] Additionlly, python support objects, modules, threads, exception handling and automatic memory management which help in modelling real world problems and building application to slove these problems.
+
+Why use python?
+  1] Python is general-purpose programming language that has simple easy-to-learn syntax that emhpasizes and therefore reduces the cost of program maintenace.
+  2] Moreover, the language is capable of scripting, is completely open source and support third-party packages encouraging modularity and reuse.
+  
+  
+  
+  
+  
+Key features:
+
+
+
+
+Some Basic Of Key Features:
+
+
+
+
+Frist Hello World Program:
